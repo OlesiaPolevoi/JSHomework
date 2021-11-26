8962740c3dbb6a3cbfc9ea6467b4024dcdaa77ed
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-11-26 00:00:02 -0500

    created variables, alert and console log

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const title = "title";
+const screens = "screens";
+const screenPrice = "screenPrice";
+const rollback = "rollback";
+const fullPrice = "fullPrice";
+const adaptive = "adaptive";
+
+alert("Any text here");
+
 console.log("Hello");
