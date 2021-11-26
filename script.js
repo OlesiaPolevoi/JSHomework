1242ac9ba8810a640d21a6b734035fc19170c9dc
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-11-26 11:48:31 -0500

    mandatory homework Lesson02 complete

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const title = "title";
-const screens = "screens";
-const screenPrice = "screenPrice";
-const rollback = "rollback";
-const fullPrice = "fullPrice";
-const adaptive = "adaptive";
+const title = "JS homework";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 8;
+const rollback = 28;
+const fullPrice = 30000000000;
+const adaptive = true;
 
-alert("Any text here");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
 
-console.log("Hello");
+console.log(`Стоимость верстки экранов ${screenPrice} юани`);
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "юани");
+
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
+console.log(screens.toLowerCase().split(" "));
+
+console.log(fullPrice * (rollback / 100));
