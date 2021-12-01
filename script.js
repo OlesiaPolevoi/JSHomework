8ec1232bbc334c99677b4d15e79b5e89d3252d21
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-11-30 20:16:25 -0500

    separated code into blocks

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 "use strict";
+
 let title = prompt("Как называется ваш проект?");
 let screens = prompt(
   "Какие типы экранов нужно разработать?",
   "Простые, Сложные, Интерактивные"
 );
 let screenPrice = +prompt("Сколько будет стоить данная работа?", "12000");
-let adaptive = !!prompt("Нужен ли адаптив на сайте?", "true/false");
-
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice1 = +prompt("Сколько это будет стоить?");
-
 let service2 = prompt("Какой дополнительный тип услуги нужен?");
 let servicePrice2 = +prompt("Сколько это будет стоить?");
-
 let rollback = 28;
-
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-
-console.log(`Стоимость верстки экранов ${screenPrice} юани`);
-console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "юани");
-
-console.log(screens.toLowerCase());
-console.log(screens.split(" "));
-console.log(screens.toLowerCase().split(" "));
-
-console.log(fullPrice * (rollback / 100));
-
 let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
-console.log(Math.ceil(servicePercentPrice));
 
-if (fullPrice > 30000) {
+if (fullPrice >= 30000) {
   console.log("Даем скидку в 10%");
-} else if (fullPrice >= 15000 && fullPrice <= 30000) {
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
   console.log("Даем скидку в 5%");
-} else if (fullPrice < 15000 && fullPrice > 0) {
+} else if (fullPrice < 15000 && fullPrice >= 0) {
   console.log("Скидка не предусмотрена");
-} else if (fullPrice <= 0) {
+} else {
   console.log("Что то пошло не так");
 }
+
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(servicePercentPrice);
+
+console.log(`Стоимость верстки экранов ${screenPrice} юани.`);
+console.log("Стоимость разработки сайта" + " " + fullPrice + " " + "юани.");
+
+// console.log(screens.toLowerCase());
+// console.log(screens.split(" "));
+// console.log(screens.toLowerCase().split(" "));
+
+// console.log(fullPrice * (rollback / 100));
+// console.log(Math.ceil(servicePercentPrice));
